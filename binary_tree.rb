4935cbde7a9787614e490f472d4c2a7b0683ee6c
ftmatsumoto <felipetmatsumoto@yahoo.com.br>
2015-08-19 11:10:16 -0300

    breadth_first_search, depth_first_search funcionando

--- binary_tree.rb
+++ binary_tree.rb
@@ -15,7 +15,7 @@ class	Tree
 	end
 
 	def build_tree(ary)
-#		ary = ary.shuffle
+		ary = ary.shuffle
 		root = Node.new(ary[0],nil,nil,nil)	
 		ary.each do |i|
 			current_node = root
@@ -48,12 +48,44 @@ class	Tree
 	  end
 	end
 
-	def breadth_first_search(value)
+	def breadth_first_search(tree, value)
+		#queue
+		queue = [tree]
+		until queue.empty?
+			current_node = queue.shift
+			return current_node.value if value == current_node.value
+			queue.push(current_node.left) if !current_node.left.nil?
+			queue.push(current_node.right) if !current_node.right.nil?
+		end
+		return -1
+	end
 
+	def depth_first_search(tree, value)
+		#stack
+		stack = [tree]
+		until stack.empty?
+			current_node = stack.pop
+			return current_node.value if value == current_node.value
+			stack.push(current_node.left) if !current_node.left.nil?
+			stack.push(current_node.right) if !current_node.right.nil?
+		end
+		return -1
+	end
+
+	def dfs_rec(current_node, value)
+		return current_node.value if value == current_node.value
+		return -1
+		dfs_rec(current_node.left, value) if !current_node.left.nil?
+		dfs_rec(current_node.right, value) if !current_node.right.nil?
 	end
 end
 
 
 array = [1,7,4,23,8,9,4,3,5,7,9,67,6345,324]
 tree = Tree.new
-tree.check_tree(tree.build_tree(array),1)
+tree_2 = tree.build_tree(array)
+tree.check_tree(tree_2)
+puts tree.breadth_first_search(tree_2,1000)
+puts tree.depth_first_search(tree_2,1000)
+puts tree.dfs_rec(tree_2,1000)
+
